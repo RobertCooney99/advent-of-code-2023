d69c4a2bbbf42d7ef1564a3ee7bd73105c6da296
Rob Cooney <robertcooney99@gmail.com>
2023-12-03 13:58:14 +0000

    AOC(002): Refactor day 2 part 1

--- solutions/day002part001.py
+++ solutions/day002part001.py
@@ -4,8 +4,6 @@ input = input.text_to_string("2")
 
 games = input.split("\n")
 
-limits = { "red": 12, "green": 13, "blue": 14 }
-
 def replace_if_higher(highest_values, amount, colour):
     if highest_values[colour] < amount:
         highest_values[colour] = amount
@@ -19,25 +17,30 @@ def is_game_possible(limits, highest_values):
             is_possible = False
     return is_possible
 
-possible_game_ids = []
+def calc_possible_games_ids(limits):
+    possible_game_ids = []
+    for game in games:
+        id, information = game.split(":")
+        id = id.split(" ")[1]
+        information = information.strip()
 
-for game in games:
-    id, information = game.split(":")
-    id = id.split(" ")[1]
-    information = information.strip()
+        handfuls = information.split(";")
 
-    handfuls = information.split(";")
+        highest_values = {"red": 0, "green": 0, "blue": 0}
 
-    highest_values = {"red": 0, "green": 0, "blue": 0}
+        for handful in handfuls:
+            cube_groups = handful.strip().split(", ")
+            for cube_group in cube_groups:
+                amount, colour = cube_group.split(" ")
+                highest_values = replace_if_higher(highest_values, int(amount), colour)
 
-    for handful in handfuls:
-        cube_groups = handful.strip().split(", ")
-        for cube_group in cube_groups:
-            amount, colour = cube_group.split(" ")
-            highest_values = replace_if_higher(highest_values, int(amount), colour)
+        if is_game_possible(limits, highest_values):
+            possible_game_ids.append(int(id))
 
-    if is_game_possible(limits, highest_values):
-        possible_game_ids.append(int(id))
+    return possible_game_ids
+
+limits = { "red": 12, "green": 13, "blue": 14 }
+possible_game_ids = calc_possible_games_ids(limits)
 
 answer = sum(possible_game_ids)
 
