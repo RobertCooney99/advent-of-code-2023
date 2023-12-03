0792664a548b90842bdd0f74401a17e452d50cba
Rob Cooney <robertcooney99@gmail.com>
2023-12-03 11:12:12 +0000

    AOC(000): Input helper functions

---
+++ solutions/utils/input.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import os
+
+def text_to_string(day, use_example = False):
+    current_path = os.path.dirname(__file__)
+    input_file_path = os.path.join(current_path, f"../../inputs/{get_input_file_name(day, '.txt', use_example)}")
+
+    return Path(input_file_path).read_text()
+
+def get_input_file_name(day, ext, use_example):
+    return (f"day{str(day).zfill(3)}{'example' if use_example else ''}{ext}")
