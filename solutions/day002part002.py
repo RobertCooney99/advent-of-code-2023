ad88c13d8a1a5c8d04c945379e0d5269135fafc7
Rob Cooney <robertcooney99@gmail.com>
2023-12-03 13:55:39 +0000

    AOC(002): Refactor day 2 part 2

--- solutions/day002part002.py
+++ solutions/day002part002.py
@@ -7,32 +7,35 @@ games = input.split("\n")
 limits = { "red": 12, "green": 13, "blue": 14 }
 
 def replace_if_higher(highest_values, amount, colour):
-    if highest_values[colour] < amount:
-        highest_values[colour] = amount
-    
+    highest_values[colour] = max(highest_values[colour], amount)
     return highest_values
-
-cube_set_powers = []
 
 def calc_cube_set_power(values):
     return values["red"] * values["green"] * values["blue"]
 
-for game in games:
-    id, information = game.split(":")
-    id = id.split(" ")[1]
-    information = information.strip()
+def calc_cube_set_powers():
+    powers = []
+    for game in games:
+        id, information = game.split(":")
+        id = id.split(" ")[1]
+        information = information.strip()
 
-    handfuls = information.split(";")
+        handfuls = information.split(";")
 
-    highest_values = {"red": 0, "green": 0, "blue": 0}
+        highest_values = {"red": 0, "green": 0, "blue": 0}
 
-    for handful in handfuls:
-        cube_groups = handful.strip().split(", ")
-        for cube_group in cube_groups:
-            amount, colour = cube_group.split(" ")
-            highest_values = replace_if_higher(highest_values, int(amount), colour)
+        for handful in handfuls:
+            cube_groups = handful.strip().split(", ")
+            for cube_group in cube_groups:
+                amount, colour = cube_group.split(" ")
+                highest_values = replace_if_higher(highest_values, int(amount), colour)
 
-    cube_set_powers.append(calc_cube_set_power(highest_values))
+        powers.append(calc_cube_set_power(highest_values))
+
+    return powers
+
+
+cube_set_powers = calc_cube_set_powers()
 
 answer = sum(cube_set_powers)
 
