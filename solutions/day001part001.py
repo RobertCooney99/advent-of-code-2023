84aa3ba97d93e38f2896d851530d70456460f7c5
Rob Cooney <robertcooney99@gmail.com>
2023-12-03 11:12:28 +0000

    AOC(001): Solution for day 001 part 001

---
+++ solutions/day001part001.py
@@ -0,0 +1,23 @@
+from utils import input
+
+input = input.text_to_string("1")
+
+lines = input.split("\n")
+
+def accumulate(start_value, list, extractor, operation):
+    result = start_value
+    for item in list:
+        result = operation(result, extractor(item))
+    return result
+
+def extract_number(line):
+    digits = ''.join(char for char in line if char.isdigit())
+    number = int(digits[0] + digits[-1])
+    return number
+
+def add(a, b):
+    return a + b
+
+answer = accumulate(0, lines, extract_number, add)
+
+print(f"Solution: {answer}")
