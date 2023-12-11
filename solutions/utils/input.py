4ad81109ddd1687b4c40f94eab25caffd7c7c95a
Rob Cooney <robertcooney99@gmail.com>
2023-12-11 15:23:15 +0000

    AOC(000): Formatting

--- solutions/utils/input.py
+++ solutions/utils/input.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 import os
 
-def text_to_string(day, use_example = False):
+
+def text_to_string(day, use_example=False):
     current_path = os.path.dirname(__file__)
-    input_file_path = os.path.join(current_path, f"../../inputs/{get_input_file_name(day, '.txt', use_example)}")
+    input_file_path = os.path.join(
+        current_path, f"../../inputs/{get_input_file_name(day, '.txt', use_example)}")
 
     return Path(input_file_path).read_text()
+
 
 def get_input_file_name(day, ext, use_example):
     return (f"day{str(day).zfill(3)}{'example' if use_example else ''}{ext}")
